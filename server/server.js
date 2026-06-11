8cda717817a3397726faa855d2cb4457d376a6ca
lalit5-dumka9 <lalitdumka08@gmail.com>
2026-06-11 23:40:21 +0530

    Fix CORS origin final

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://personal-task-manager-indol-six.vercel.app",
+      
+  })
+);
 
 app.use(express.json());
 
